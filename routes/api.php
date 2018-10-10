b056664abb98d1c0c579ebcc0ada3e9e9178ef26
Yaneth Ramirez <yanethi28@gmail.com>
2018-10-10 19:36:48 -0400

    Creación de Rutas de Recurso para cada Controlador

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,21 @@ use Illuminate\Http\Request;
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
+
+Route::resource('compradores','Comprador\CompradorController',
+                ['only' => ['index','show']]);
+
+Route::resource('categorias','Categoria\CategoriaController',
+                ['except' => ['create','edit']]);
+
+Route::resource('productos','Productos\ProductosController',
+                ['only' => ['index','show']]);
+
+Route::resource('transacciones','Transaccion\TransaccionController',
+                ['only' => ['index','show']]);
+
+Route::resource('vendedores','Vendedor\VendedorController',
+                ['only' => ['index','show']]);
+
+Route::resource('user','User\UserController',
+                ['except' => ['create','edit']]);
