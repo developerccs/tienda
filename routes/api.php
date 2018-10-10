4b0b3c76cf4ed7307821d574ce275e3e83ea0b00
Yaneth Ramirez <yanethi28@gmail.com>
2018-10-10 18:44:54 -0400

    Creación de la estructura inicial para los modelos y sus migraciones

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*middleware auth y guard api ('auth:api')*/
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
