3695e505672ca67da68ac8d10a6002e3327c1d00
Yaneth Ramirez <yanethi28@gmail.com>
2018-10-10 20:33:02 -0400

    Implementando las propiedades de los recursos categoria y producto

--- app/Producto.php
+++ app/Producto.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
-    //
+    const PRODUCTO_DISPONIBLE = 'DISPONIBLE';
+    const PRODUCTO_NO_DISPONIBLE = 'NO DISPONIBLE';
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'cantidad',
+        'estatus',
+        'imagen',
+        'vendedor_id'
+    ];
+
+    public function disponibilidad()
+    {
+        return $this->estatus == Producto::PRODUCTO_DISPONIBLE;
+    }
 }
