811fb74bb7e35f479177f4ca2ef72f6df7165c60
Yaneth Ramirez <yanethi28@gmail.com>
2018-10-11 23:46:58 -0400

    Implementado los métodos index y show en TransaccionController

--- app/Http/Controllers/Transaccion/TransaccionController.php
+++ app/Http/Controllers/Transaccion/TransaccionController.php
@@ -2,84 +2,22 @@
 
 namespace App\Http\Controllers\Transaccion;
 
+use App\Transaccion;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
 
 class TransaccionController extends ApiController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        $transacciones = Transaccion::all();
+        return $this->showAll($transacciones);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function show(Transaccion $transaccione)
     {
-        //
+        return $this->showOne($transaccione);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
