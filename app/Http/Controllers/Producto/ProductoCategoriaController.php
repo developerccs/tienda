3134cf24dbcf48cf3aedc2409e409e3fa3675cd0
Yaneth Ramirez <yanethi28@gmail.com>
2018-10-12 15:50:23 -0400

    Implementado operacion metodo update ProductoCategoriaController

--- app/Http/Controllers/Producto/ProductoCategoriaController.php
+++ app/Http/Controllers/Producto/ProductoCategoriaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Producto;
 
 use App\Producto;
+use App\Categoria;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
 
@@ -16,9 +17,11 @@ class ProductoCategoriaController extends ApiController
         
     }
 
-    public function update(Request $request, Producto $producto)
+    public function update(Request $request, Producto $producto, Categoria $categoria)
     {
-  
+        $producto->categorias()->syncWithoutDetaching([$categoria->id]);
+
+        return $this->showAll($producto->categorias);
     }
 
     public function destroy( Producto $producto)
