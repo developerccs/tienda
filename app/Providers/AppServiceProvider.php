086c50f78a6ff5349439ee440ab256e06bf0c69e
Yaneth Ramirez <yanethi28@gmail.com>
2018-10-12 16:24:48 -0400

    Implementado Evento Producto Disponible o No Disponible

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Producto;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,6 +16,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        Producto::updated(function($producto) {
+            if ($producto->cantidad == 0 && $producto->disponibilidad()) {
+                $producto->estatus = Producto::PRODUCTO_NO_DISPONIBLE;
+                $producto->save();
+            }
+        });
     }
 
     /**
