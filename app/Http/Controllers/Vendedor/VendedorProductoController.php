0320430e72b022d245633f51af6dfc6b2076b5ce
Yaneth Ramirez <yanethi28@gmail.com>
2018-10-12 13:32:48 -0400

    Implementado operacion metodo store VendedorProductoController

--- app/Http/Controllers/Vendedor/VendedorProductoController.php
+++ app/Http/Controllers/Vendedor/VendedorProductoController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Vendedor;
 
 use App\Vendedor;
+use App\User;
+use App\Producto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Controllers\ApiController;
 
 class VendedorProductoController extends ApiController
@@ -13,6 +16,28 @@ class VendedorProductoController extends ApiController
         $productos = $vendedore->productos;
 
         return $this->showAll($productos);
+    }
+
+    public function store(Request $request, User $vendedore)
+    {
+        $reglas = [
+            'nombre' => 'required',
+            'descripcion' => 'required',
+            'cantidad' => 'required|integer|min:1',
+            'imagen' => 'required|image',
+        ];
+
+        $this->validate($request, $reglas);
+
+        $data = $request->all();
+
+        $data['estatus'] = Producto::PRODUCTO_NO_DISPONIBLE;
+        $data['imagen'] = '1.jpg';
+        $data['vendedor_id'] = $vendedore->id;
+
+        $producto = Producto::create($data);
+
+        return $this->showOne($producto, 201);
     }
 }
 
